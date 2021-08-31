f3d2788eb8cab765a396d6ff20923b13f551ca3f
fatoshalimi <54471283+fatoshalimi@users.noreply.github.com>
2021-08-31 15:36:35 +0200

    hugo install

--- action.sh
+++ action.sh
@@ -18,11 +18,12 @@ echo '🔧 Install tools'
 npm init -y && npm install -y postcss postcss-cli autoprefixer
 
 echo '🤵 Install Hugo'
-HUGO_VERSION=$(curl -s https://api.github.com/repos/gohugoio/hugo/releases/latest | jq -r '.tag_name')
-mkdir tmp/ && cd tmp/
-curl -sSL https://github.com/gohugoio/hugo/releases/download/${HUGO_VERSION}/hugo_extended_${HUGO_VERSION: -6}_Linux-64bit.tar.gz | tar -xvzf-
-mv hugo /usr/local/bin/
-cd .. && rm -rf tmp/
+# HUGO_VERSION=$(curl -s https://api.github.com/repos/gohugoio/hugo/releases/latest | jq -r '.tag_name')
+# mkdir tmp/ && cd tmp/
+# curl -sSL https://github.com/gohugoio/hugo/releases/download/${HUGO_VERSION}/hugo_extended_${HUGO_VERSION: -6}_Linux-64bit.tar.gz | tar -xvzf-
+# mv hugo /usr/local/bin/
+# cd .. && rm -rf tmp/
+apt-get install hugo
 cd ${GITHUB_WORKSPACE}
 hugo version || exit 1
 
